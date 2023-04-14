1e7faada52a1c889af95481f1e800a2ebda07462
Admin <Admin@YzetaPC>
2023-04-14 18:28:28 +0200

    Segundo commit con nuevos cambios

--- EclipseGit1/src/Proyecto1/Principal1.java
+++ EclipseGit1/src/Proyecto1/Principal1.java
@@ -2,6 +2,8 @@ package Proyecto1;
 
 public class Principal1 {
 	public void main(String[] args) {
-		System.out.print("Hola git");
+		System.out.println("Hola git");
+		System.out.print("Tengop que hacer ");
+		System.out.print("un par de cambios");
 	}
 }
